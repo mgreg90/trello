05fd0ea3cd016e6152d1b62ecbee4a9d24322ee1
Mike Gregory <mgregory8219@gmail.com>
2018-04-02 23:08:09 -0400

    Updated script to use inline gemfile.

--- trello.rb
+++ trello.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
+require 'bundler/inline'
+gemfile do
+  source 'https://rubygems.org'
+  gem 'playwright-cli', require: 'playwright/cli'
+end
 
-require 'playwright/cli'
 require 'uri'
 
 require_relative 'lib/version'
